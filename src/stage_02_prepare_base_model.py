e9e61ed07ae79caa6b2ec5a6033012f2728226b0
c17hawke <2016peb5126@mnit.ac.in>
2021-10-11 20:15:17 +0530

    stage 02 main file is ready

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -1,7 +1,7 @@
 import argparse
 import os
-import shutil
-from types import prepare_class
+from src.utils.common import read_yaml_file, create_directories
+from src.utils.model import get_VGG16_model, prepare_full_model
 from tqdm import tqdm
 import logging
 
@@ -13,18 +13,40 @@ logging.basicConfig(
     filemode="a"
     )
 
-def prepare_base_model(config_path):
-    pass
+def prepare_base_model(config_path: str, params_path: str) -> None:
+    config = read_yaml_file(config_path)
+    params = read_yaml_file(params_path)
+
+    artifacts = config["artifacts"]
+    artifacts_dir = config["ARTIFACTS_DIR"]
+
+    base_model_dir = artifacts["BASE_MODEL_DIR"]
+    base_model_name = artifacts["BASE_MODEL_NAME"]
+
+    base_model_dir_path = os.path.join(artifacts_dir, base_model_dir)
+    create_directories([base_model_dir_path])
+
+    base_model_path = os.path.join(base_model_dir_path, base_model_name)
+
+    base_model = get_VGG16_model()
+
+    full_model = prepare_full_model(base_model)
+
+    updated_base_model_path = os.path.join(base_model_dir_path, artifacts["UPDATED_BASE_MODEL_NAME"])
+
+    full_model.save(updated_base_model_path)
+
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info("\n********************")
         logging.info(">>>>> stage two started <<<<<")
-        prepare_base_model(config_path=parsed_args.config)
+        prepare_base_model(config_path=parsed_args.config, params_path=parsed_args.params)
         logging.info(">>>>> stage two completed! base model is created and saved successfully <<<<<\n")
     except Exception as e:
         logging.exception(e)
---
+++ src/utils/model.py
@@ -0,0 +1,8 @@
+
+
+def get_VGG16_model():
+    return None
+
+
+def prepare_full_model(base_model):
+    return None
