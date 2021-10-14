7444d4bc61ac2f25d2cdbdc90065ba4f5841c4d6
c17hawke <2016peb5126@mnit.ac.in>
2021-10-15 04:20:28 +0530

    callback docstring update

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -52,7 +52,15 @@ def create_and_save_checkpointing_callback(callbacks_dir: str, checkpoint_dir: s
     logging.info(f"checkpoint callback is saved at {ckpt_callback_filepath} as binary file")
 
 
-def get_callbacks(callback_dir_path: str):
+def get_callbacks(callback_dir_path: str)-> list:
+    """load saved callbacks from the callbacks directory
+
+    Args:
+        callback_dir_path (str): path to callback_dir
+
+    Returns:
+        list: list of callbacks for training
+    """
     
     callback_paths = [
         os.path.join(callback_dir_path, pickle_file) for pickle_file in os.listdir(callback_dir_path) if pickle_file.endswith(".cb")
@@ -61,6 +69,6 @@ def get_callbacks(callback_dir_path: str):
         joblib.load(path) for path in callback_paths
     ]
 
-    logging.info(f"saved callbacks are now ready to be used")
+    logging.info(f"saved callbacks are loaded and now ready to be used")
     
     return callbacks
